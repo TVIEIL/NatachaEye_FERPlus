08e57aa21004a14d8414de0ede1f267eeebdffc8
TVIEIL <tvieil59@gmail.com>
2026-05-25 20:53:29 +0200

    chore : remove accidental /dev directory from tracking

--- dev/opencv_onnx_cuda_test.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import onnxruntime
-import torch
-print("OpenCV version:", cv2.__version__)
-print("ONNX Runtime version:", onnxruntime.__version__)
-print("CUDA disponible:", torch.cuda.is_available())
